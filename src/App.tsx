b53d7aa0dbad9be0658bd59bde91d21f4ecb833f
Karuna <kneerukonda@tmhcc.com>
2021-11-04 21:21:08 -0500

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Counter from './Counter';
 function App() {
   return (
     <div className="App">
-       
+       <h1 style={{textAlign:'center'}}>Welcome to our Counter app</h1>
         <Counter/>
     </div>
   );
